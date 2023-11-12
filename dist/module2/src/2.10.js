08176551ba2df5e66fdef19feceafbcb801afbcc
jahid <jahidmorol2@gmail.com>
2023-11-12 12:13:32 +0600

    m-10 done

--- dist/module2/src/2.10.js
+++ dist/module2/src/2.10.js
@@ -1,1 +1,12 @@
 "use strict";
+{
+    //
+    // mapped types
+    const arrOfNumbers = [1, 4, 5];
+    // const arrOfStrings : string[] = ['1','4','5']
+    const arrOfStrings = arrOfNumbers.map((number) => number.toString());
+    const area1 = {
+        hight: '450px',
+        width: 100
+    };
+}
